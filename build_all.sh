166b4fb5ead7acfe6f8a84f88af70427ead3f82a
potatolain <sarah@igwgames.com>
2024-08-05 00:09:44 -0400

    heck it, do this the easy way

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-EMCC_ARGS="-s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS=\"['FS', 'callMain']\" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0"
 
 cd ./shiru-tools/nesasmc
-emcc nesasmc.cpp -o nesasmc.js $EMCC_ARGS
+emcc nesasmc.cpp -o nesasmc.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
 cd ../nsf2data
-emcc nsf2data.cpp -o nsf2data.js $EMCC_ARGS
+emcc nsf2data.cpp -o nsf2data.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
 cd ../text2data
-emcc text2datac.cpp -o text2data.js $EMCC_ARGS
+emcc text2datac.cpp -o text2data.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
