c4de84429744191382b34de65df392d21c9e1d31
potatolain <sarah@igwgames.com>
2024-08-05 00:26:58 -0400

    Forgot which lang this was

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Usage: build_tool filename
-function build_tool() {
+function build_tool () {
     cd $1
     emcc $1.cpp -o $1.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
     cp $1.js ../../dist
