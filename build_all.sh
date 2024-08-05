813ea4f2a85a687ad19f59d3f64da885bb0f7c58
potatolain <sarah@igwgames.com>
2024-08-05 00:09:40 -0400

    no pushpop

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 EMCC_ARGS="-s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS=\"['FS', 'callMain']\" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0"
 
-pushd
 cd ./shiru-tools/nesasmc
 emcc nesasmc.cpp -o nesasmc.js $EMCC_ARGS
 cd ../nsf2data
 emcc nsf2data.cpp -o nsf2data.js $EMCC_ARGS
 cd ../text2data
 emcc text2datac.cpp -o text2data.js $EMCC_ARGS
-popd
