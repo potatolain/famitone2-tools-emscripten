91099dff887bdc1ea73d5b551ec0d40a4ca9d8d8
potatolain <sarah@igwgames.com>
2024-08-05 00:13:02 -0400

    typo

--- build_all.sh
+++ build_all.sh
@@ -5,4 +5,4 @@ emcc nesasmc.cpp -o nesasmc.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS',
 cd ../nsf2data
 emcc nsf2data.cpp -o nsf2data.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
 cd ../text2data
-emcc text2datac.cpp -o text2data.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
+emcc text2data.cpp -o text2data.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
