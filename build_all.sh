001c9e8cc8b4ee7ebd9acbb09222dfefbbc1ec65
potatolain <sarah@igwgames.com>
2024-08-05 00:29:14 -0400

    Status info

--- build_all.sh
+++ build_all.sh
@@ -2,11 +2,13 @@
 
 # Usage: build_tool filename
 build_tool () {
+    echo Starting build of $1
     cd $1
     emcc $1.cpp -o $1.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
     cp $1.js ../../dist
     cp $1.wasm ../../dist
     cd ..
+    echo Build of $1 completed successfully.
 }
 mkdir -p dist
 cd shiru-tools
