576bca7b712aec5b08a3ed7c7f5fcc6e92032609
potatolain <sarah@igwgames.com>
2024-08-07 00:20:24 -0400

    Try to make it work again

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,7 @@
 build_tool () {
     echo Starting build of $1
     cd $1
-    emcc $1.cpp -o $1.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0 -s EXIT_RUNTIME=1
+    emcc $1.cpp -o $1.js -s USE_ZLIB=1 -s EXPORTED_RUNTIME_METHODS="['FS', 'callMain']" -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME='createModule' -s USE_ES6_IMPORT_META=0
     cp $1.js ../../dist/emscripten
     cp $1.wasm ../../dist/emscripten
     cd ..
